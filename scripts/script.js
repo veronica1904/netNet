fdef59702c9bf628a195f5ef7b7603b1db3dd20e
Jarling Duque <jarlingduqueduarte@gmail.com>
2022-02-11 21:28:43 -0500

    create version movil disaing netNet

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// let text = "";
+// const fruits = ["apple", "orange", "cherry"];
+// fruits.forEach(myFunction);
+
+// document.getElementById("demo").innerHTML = text;
+ 
+// function myFunction(item, index) {
+//   text += index + ": " + item + "<br>"; 
+// }
